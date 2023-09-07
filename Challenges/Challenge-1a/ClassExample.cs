b1cb5a438827bea603e241db610308e40cf0ac33
Chris Tate <143023706+KoRnyM3mes@users.noreply.github.com>
2023-09-07 13:08:00 -0600

    Added ClassExample

---
+++ Challenges/Challenge-1a/ClassExample.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace classexamples
+{
+    public class classwithvariables
+    {
+        public int num = 2;
+        public string name = "Crust";
+        public bool IsThisCodeSimple = true;
+    }
+
+    public class classwithfunctions
+    {
+        public void Main(string[] args)
+        {
+             Console.WriteLine("Running Function");
+             Console.WriteLine("Program is functional!");
+        }
+    }
+}
