868df92f53a964800a92cd9158c0ab994cf6cf77
Christopher Tate <CrustRust15@gmail.com>
2023-09-23 13:48:46 -0600

    updated favorite foods
    now has user input

--- Challenges/Challenge-1c/FavoriteFood.cs
+++ Challenges/Challenge-1c/FavoriteFood.cs
@@ -5,10 +5,17 @@ public class FoodFave
     public void Main()
     {
         Console.WriteLine("What is your top 3 favorite foods?"); 
-        string[] foods = {"Hamburger", "Pizza", "Olives"}; // input foods here
+
+        string[] foods = new string[3]; //array for foods
+
+        foods[0] = Console.ReadLine();
+        foods[1] = Console.ReadLine();
+        foods[2] = Console.ReadLine();
+
         foreach (string i in foods)
         {
             Console.WriteLine("I love " + i + "!"); //Writes the same message for each item in the array
         }
+        Console.ReadKey();
     }
 }
